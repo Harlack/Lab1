31204f8422a2d1f9958eed0120139e945b717884
Jan kowalski <jan.kowalski@pollub.edu.pl>
2023-10-05 15:55:58 +0200

    pollub-3

---
+++ src/Product.java
@@ -0,0 +1,8 @@
+public class Product {
+    public void getProduct(String product){
+        System.out.println("Produkt: " + product);
+    }
+    public void addProduct(String product){
+        System.out.println("Dodano produkt: " + product);
+    }
+}
